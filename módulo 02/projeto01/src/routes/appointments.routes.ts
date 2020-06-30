a23f8544355f15d547bb5b93d95298953706ff31
Softilux7 <66313209+Softilux7@users.noreply.github.com>
2020-06-30 17:37:51 -0300

    Desafio 05

--- módulo 02/projeto01/src/routes/appointments.routes.ts
+++ módulo 02/projeto01/src/routes/appointments.routes.ts
@@ -1,37 +1,34 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, parse, isEqual } from 'date-fns';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
+appointmentsRouter.get('/', (request, response) => {
+  const appointment = appointmentsRepository.all();
 
-const appointments: Appointment[] = [];
+  return response.json(appointment);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
-  const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
-  );
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+    const CreateAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+    const appointment = CreateAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
-  appointments.push(appointment);
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
